7cd7b7e5aa98f7de511ccadc7b80b65700e9f20f
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-02-21 18:25:43 -0500

    Updates * Added Command Line Arguments * Added Seperator Symbol Flag to use custom delimiters * Bug Fixes * Removed Example Page

--- MorseTool.java
+++ MorseTool.java
@@ -3,13 +3,88 @@ import java.util.Map;
 
 public class MorseTool{
 
-	private Map<String,Character> usMorseTable;
-	private Map<Character,String> reverseTable;
-	private final Character BAD_CHAR = '*';
+	private final static Character BAD_CHAR = '*';
 	
-	public MorseTool(){
+	private static Map<String,Character> usMorseTable;
+	private static Map<Character,String> reverseTable;
+	private static String seperator = "/";
+	
+	public static void main(String[] args) {
+				
+		if(args.length == 1){
+			
+			if(args[0].toUpperCase().equals("-HELP")){
+				System.out.println("Help and Usage");
+				System.out.println("-seperator / -encrypt \"THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG\"");
+				System.out.println("-seperator /-unencrypt \"- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. ... / --- ...- . .-. / - .... . / .-.. .- --.. -.-- / -.. --- --.\"");
+				System.out.println("-replace / # \"- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. ... / --- ...- . .-. / - .... . / .-.. .- --.. -.-- / -.. --- --.\"");
+			}
+		}
+			
+		int count = -1;
+	
+		int encryptPos = -1;
+		int decryptPos = -1;
+		int replacePos = -1;
+		int sepPos = -1;
+		
+		if(args.length != 0){
+			
+			for(String arg : args){
+				
+				count++;
+				
+				/* Flags */
+				
+				if(arg.toUpperCase().equals("-SEPERATOR"))
+					sepPos = count + 1;
+				
+				if(arg.toUpperCase().equals("-ENCRYPT"))
+					encryptPos = count + 1;
+				
+				if(arg.toUpperCase().equals("-UNENCRYPT"))
+					decryptPos = count + 1;
+				
+				if(arg.toUpperCase().equals("-REPLACE"))
+					replacePos = count + 1;
+				
+				/* Arguments */
+				
+				if(sepPos == count){						
+					Setup(arg.charAt(0) + "");
+					System.out.println("-seperator set as " + arg.charAt(0));
+				}			
+				
+				if(encryptPos == count){	
+					Setup(seperator);
+					String result = encrypt(arg);
+					System.out.println(result);
+				}		
+				
+				if(decryptPos == count){	
+					Setup(seperator);
+					String result = unencrypt(arg);
+					System.out.println(result);
+				}		
+				
+				if(replacePos == count){						
+					
+					if(count + 3 >= args.length){	
+						Setup(seperator);						
+						String result = replaceSeperatorChar(args[count + 2], args[count + 1], args[count]);
+						System.out.println(result);
+					}
+				}	
+			}
+		}
+	}
+	
+	/* Constructors */
+	
+	private static void Setup(String symbol){
+		
 		usMorseTable = new HashMap<String,Character>();
-		reverseTable = new HashMap<Character, String>();
+		reverseTable = new HashMap<Character,String>();
 		
 		usMorseTable.put(".-", 'A');
 		usMorseTable.put("-...", 'B');
@@ -38,16 +113,33 @@ public class MorseTool{
 		usMorseTable.put("-.--", 'Y');
 		usMorseTable.put("--..", 'Z');	
 		usMorseTable.put("..--..", '.');
-		usMorseTable.put("/", ' ');
+		
+		seperator = symbol;
+		
+		usMorseTable.put(seperator, ' ');
 		
 		for(String s : usMorseTable.keySet())
 			reverseTable.put(usMorseTable.get(s), s);
 	}
 	
-	public String translateToMorse(String longText) {
+	/* Methods */
+	
+	private static String replaceSeperatorChar(String morseText, String seperator, String oldseperator){
+		
+		String replacedText = morseText;
+		
+		String replaceMe = oldseperator.charAt(0) + "";
+		String withMe = seperator.charAt(0) + "";
+		
+		replacedText = replacedText.replaceAll(replaceMe, withMe);
+		
+		return replacedText;
+	}
+	
+	private static String encrypt(String plainText) {
 		
 		StringBuffer sb = new StringBuffer();
-		String[] lt = longText.split(" ");
+		String[] lt = plainText.split(" ");
 		String morse;
 		
 		for(String s : lt){			
@@ -56,35 +148,38 @@ public class MorseTool{
 				sb.append(morse + " ");
 			}		
 			
-			sb.append("/ ");
+			sb.append(seperator + " ");
 		}
 		
 		return sb.toString();
 	}	
 	
-	public String translateRawMorse(String longText) {
+	private static String unencrypt(String longText) {
 		
 		StringBuffer sb = new StringBuffer();
 		String[] lt = longText.split(" ");
-		Character c;
+		Character c = ' ';
 		
 		for(String s : lt){			
-			c = morseCharToReadable(s);			
-			sb.append(c);
+			
+			if(!s.isEmpty()){
+				c = morseCharToReadable(s);		
+				sb.append(c);
+			}
 		}
 		
 		return sb.toString();
 	}
 	
-	public Character morseCharToReadable(String mc) {
+	private static Character morseCharToReadable(String mc) {
 		
 		if(usMorseTable.containsKey(mc))
 			return usMorseTable.get(mc);
 		
-		return BAD_CHAR;
+		return ' ';
 	}	
 	
-	public String readableToMorseChar(Character mc) {
+	private static String readableToMorseChar(Character mc) {
 		
 		Character c = Character.toUpperCase(mc);
 		
